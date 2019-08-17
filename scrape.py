e1b7eb804c29d96b77a58bd3fe73b797ec42bdf6
Devon Navon <devonnavon@MacBook-Pro.lan1>
2019-08-17 09:16:31 -0500

    working city search and scraper

--- scrape.py
+++ scrape.py
@@ -1,24 +1,63 @@
 import requests
-import certifi
 from bs4 import BeautifulSoup
 from ast import literal_eval
 
-def single(url):
+yelp_url = 'https://www.yelp.com/'
+
+
+def get_city_search(search, city, state, page_limit = 1):
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+    url = yelp_url + 'search?find_desc=' + search + '&find_loc=' + city + '%2C%20' + state + '&start={}'
+
+    for i in range(0,page_limit*30,30):
+        response = requests.get(url.format(i), headers=headers, verify=False).text
+        page = BeautifulSoup(response, "lxml")
+        if i == 0:
+            links = []
+            names = []
+        #iterate through list of links within <h3> tags
+        for y in [x.select_one('a[href*=/biz/]') for x in page.find_all('h3')]:
+            if y is not None: #filter out h3s that don't have links
+                links.append(y.attrs['href']) #append link attribute
+                names.append(y.text.strip())
+    return {'names': names,'links':links}
+
+
+def get_header(page):
+    return page.find('div',attrs={'class': 'content-container js-biz-details'})
+
+#def get_info(page):
+
+
+def get_reviews(url, page_limit = 1):
+    '''
+    url is shorthand yelp business url
+    example: url = /biz/the-roosevelt-room-austin'
+    funciton appends {} to front of parameter url
+    page_limit = number of requests to iterate through reviews
+                 number of reviews = page_limit * 20
+    '''
     #Headers will make it look like you are using a web browser
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
     reviews = []
-    yelp = 'https://www.yelp.com/'
-    final_url = yelp + url.split('?')[0] + '?start={}&sort_by=date_desc'
+    final_url = yelp_url + url + '?start={}&sort_by=date_desc'
 
-    for i in range(0,500,20):
-        final_url.format(i)
-        response = requests.get(url, headers=headers, verify=False).text
+    for i in range(0,page_limit*20,20):
+        response = requests.get(final_url.format(i), headers=headers, verify=False).text
         page = BeautifulSoup(response, "lxml")
         #on the first request we get business data
         if i == 0:
             header = page.find('div',attrs={'class': 'content-container js-biz-details'})
-            name = header.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white shortenough'})[1].text
-            price = header.find('span',attrs={'class':'business-attribute price-range'}).text
+            name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white shortenough'})
+            if len(name_raw) == 0:
+                name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white'})
+            else:
+                pass
+            name = ' '.join([x.text for x in name_raw]).strip()
+            try: #some places don't have a price
+                price = header.find('span',attrs={'class':'business-attribute price-range'}).text
+            except AttributeError:
+                price = None
             ratings_raw = literal_eval(header.find('div', attrs={'id':"rating-details-modal-content"}).attrs['data-monthly-ratings'])
             ratings = {}
             for x in ratings_raw:
