c5a620328adf5032109f8ecd89282ef91165fb6f
Devon Navon <devonnavon@MacBook-Pro.lan1>
2019-08-17 09:45:16 -0500

    cleaned up get_reviews with function renames get_business and search to get_city

--- scrape.py
+++ scrape.py
@@ -4,8 +4,7 @@ from ast import literal_eval
 
 yelp_url = 'https://www.yelp.com/'
 
-
-def get_city_search(search, city, state, page_limit = 1):
+def get_city(search, city, state, page_limit = 1):
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
     url = yelp_url + 'search?find_desc=' + search + '&find_loc=' + city + '%2C%20' + state + '&start={}'
 
@@ -26,10 +25,55 @@ def get_city_search(search, city, state, page_limit = 1):
 def get_header(page):
     return page.find('div',attrs={'class': 'content-container js-biz-details'})
 
-#def get_info(page):
+def get_name(page):
+    name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white shortenough'})
+    if len(name_raw) == 0:
+        name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white'})
+    else:
+        pass
+    name = ' '.join([x.text for x in name_raw]).strip()
+    return name
 
+def get_price(header):
+    try: #some places don't have a price
+        price = header.find('span',attrs={'class':'business-attribute price-range'}).text
+    except AttributeError:
+        price = None
+    return price
 
-def get_reviews(url, page_limit = 1):
+def get_ratings(header):
+    ratings_raw = literal_eval(header.find('div', attrs={'id':"rating-details-modal-content"}).attrs['data-monthly-ratings'])
+    ratings = {}
+    for x in ratings_raw:
+        ratings.update({int(x) : [y[1] for y in ratings_raw[x]]})
+    return ratings
+
+def get_hours(page):
+    try: #some places don't have hours
+        hours = [dict([x.text.strip().split('\n\n')[0:2]]) for x in page.find('table',attrs={'class':'table table-simple hours-table'}).find_all('tr')]
+    except AttributeError:
+        hours = None
+    return hours
+
+def get_info(page):
+    info_raw = page.find('div',attrs={'class':'short-def-list'})
+    info = dict(zip([x.text.strip() for x in info_raw.find_all('dt')], [x.text.strip() for x in info_raw.find_all('dd')]))
+    return info
+
+def get_reviews(page):
+    for p in page.find_all("div", attrs={'class': 'review-content'}):
+        date = p.find('span', attrs={'class': 'rating-qualifier'}).text.strip()
+        rating = float(p.find('img').attrs['alt'][:3])
+        content = p.find('p', attrs={'lang': 'en'}).text
+        review = {
+            'date' : date,
+            'rating' : rating,
+            'content' : content
+        }
+        reviews.append(review)
+    return reviews
+
+def get_business(url, page_limit = 1):
     '''
     url is shorthand yelp business url
     example: url = /biz/the-roosevelt-room-austin'
@@ -37,47 +81,27 @@ def get_reviews(url, page_limit = 1):
     page_limit = number of requests to iterate through reviews
                  number of reviews = page_limit * 20
     '''
-    #Headers will make it look like you are using a web browser
+    #headers will make it look like you are using a web browser
     headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
     reviews = []
     final_url = yelp_url + url + '?start={}&sort_by=date_desc'
-
     for i in range(0,page_limit*20,20):
         response = requests.get(final_url.format(i), headers=headers, verify=False).text
         page = BeautifulSoup(response, "lxml")
         #on the first request we get business data
         if i == 0:
-            header = page.find('div',attrs={'class': 'content-container js-biz-details'})
-            name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white shortenough'})
-            if len(name_raw) == 0:
-                name_raw = page.find_all('h1',attrs={'class': 'biz-page-title embossed-text-white'})
-            else:
-                pass
-            name = ' '.join([x.text for x in name_raw]).strip()
-            try: #some places don't have a price
-                price = header.find('span',attrs={'class':'business-attribute price-range'}).text
-            except AttributeError:
-                price = None
-            ratings_raw = literal_eval(header.find('div', attrs={'id':"rating-details-modal-content"}).attrs['data-monthly-ratings'])
-            ratings = {}
-            for x in ratings_raw:
-                ratings.update({int(x) : [y[1] for y in ratings_raw[x]]})
-            hours = [dict([x.text.strip().split('\n\n')[0:2]]) for x in page.find('table',attrs={'class':'table table-simple hours-table'}).find_all('tr')]
-            info_raw = page.find('div',attrs={'class':'short-def-list'})
-            info = dict(zip([x.text.strip() for x in info_raw.find_all('dt')], [x.text.strip() for x in info_raw.find_all('dd')]))
-        else: pass
-        for p in page.find_all("div", attrs={'class': 'review-content'}):
-            date = p.find('span', attrs={'class': 'rating-qualifier'}).text.strip()
-            rating = float(p.find('img').attrs['alt'][:3])
-            content = p.find('p', attrs={'lang': 'en'}).text
-            review = {
-                'date' : date,
-                'rating' : rating,
-                'content' : content
-            }
-            reviews.append(review)
+            name = get_name(page)
+            header = get_header(page)
+            price = get_price(header)
+            ratings = get_ratings(header)
+            hours = get_hours(page)
+            info = get_info(page)
+        else:
+            pass
+        #on all page requests we get reviews
+        reviews = get_reviews(page)
 
-    scrape = {
+    business = {
         'name':name,
         'price':price,
         'ratings':ratings,
@@ -85,4 +109,4 @@ def get_reviews(url, page_limit = 1):
         'info':info,
         'reviews':reviews
     }
-    return scrape
+    return business
